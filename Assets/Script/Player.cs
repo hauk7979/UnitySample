24e6e54f0508a52d9445c573cbad07cc0e187989
hauk79 <hauk@kw.ac.kr>
2023-06-16 11:08:41 +0900

    큐브의 공중 점프 방지

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 namespace Script
@@ -7,18 +8,28 @@ namespace Script
         private Rigidbody _rigidbody;
         private const float JumpSpeed = 5.0f;
 
+        public bool isLanding = false;
+        
         // Start is called before the first frame update
         private void Start()
         {
             _rigidbody = GetComponent<Rigidbody>();
+            isLanding = false;
         }
 
         // Update is called once per frame
         private void Update()
         {
-            if (Input.GetMouseButtonDown(0)) {
+            if (isLanding && Input.GetMouseButtonDown(0))
+            {
+                isLanding = false;
                 _rigidbody.velocity = Vector3.up * JumpSpeed;
             }
+        }
+
+        private void OnCollisionEnter(Collision other)
+        {
+            isLanding = true;
         }
     }
 }
