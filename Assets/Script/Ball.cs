f73d92a4491bae5624e36d834963594554c4a79f
hauk79 <hauk@kw.ac.kr>
2023-06-16 10:46:24 +0900

    fly ball

---
+++ Assets/Script/Ball.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace Script
+{
+    public class Ball : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        private void Start()
+        {
+            GetComponent<Rigidbody>().velocity = new Vector3(-8.0f, 8.0f, 0.0f);
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        
+        }
+    }
+}
