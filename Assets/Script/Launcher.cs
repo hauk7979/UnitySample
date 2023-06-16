67af58b88c2e1109d54bf346fcff8ee591af8fe9
hauk79 <hauk@kw.ac.kr>
2023-06-16 10:59:47 +0900

    볼 생성 스크립트

---
+++ Assets/Script/Launcher.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+namespace Script
+{
+    public class Launcher : MonoBehaviour
+    {
+        public GameObject ballPrefab;
+        
+        // Start is called before the first frame update
+        private void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetMouseButtonDown(1))
+            {
+                Instantiate(ballPrefab);
+            }
+        }
+    }
+}
